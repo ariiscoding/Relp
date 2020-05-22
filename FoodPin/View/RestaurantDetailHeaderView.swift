def7c39ad3729c5cbf7c22d992886d65b6a312d5
Ari He <ariqhe@gmail.com>
2020-05-22 16:26:09 -0700

    Finished the header in detail view

--- FoodPin/View/RestaurantDetailHeaderView.swift
+++ FoodPin/View/RestaurantDetailHeaderView.swift
@@ -11,16 +11,33 @@ import UIKit
 class RestaurantDetailHeaderView: UIView {
     
     @IBOutlet var headerImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var typeLabel: UILabel!
-    @IBOutlet var heartImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel! {
+        //enable  atuomatically determine total number of lines to fit content
+        didSet {
+            nameLabel.numberOfLines = 0 // 0 means automatic
+        }
+    }
+    @IBOutlet var typeLabel: UILabel! {
+        //to enable rounded corner
+        didSet {
+            typeLabel.layer.cornerRadius = 5.0
+            typeLabel.layer.masksToBounds = true 
+        }
+    }
+    @IBOutlet var heartImageView: UIImageView!{
+        //load the image as a template so we can change its color
+        didSet {
+            heartImageView.image = UIImage(named: "heart-tick")?.withRenderingMode(.alwaysTemplate)
+            heartImageView.tintColor = .white
+        }
+    }
     
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
 }
