d383153a47be37516199d0d91fa5f47e471a3b41
Ari He <ariqhe@gmail.com>
2020-05-24 02:35:01 -0700

    Ugh failed

--- FoodPin/View/RestaurantTableViewCell.swift
+++ FoodPin/View/RestaurantTableViewCell.swift
@@ -4,8 +4,12 @@ import UIKit
 class RestaurantTableViewCell: UITableViewCell {
 
     @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var locationLabel: UILabel!
-    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel! 
+    @IBOutlet var typeLabel: UILabel! {
+        didSet {
+            nameLabel.numberOfLines=0
+        }
+    }
     @IBOutlet var thumbnailImageView: UIImageView!
     @IBOutlet var heartImageView: UIImageView!
     
