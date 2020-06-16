c78bef17e1e7080448d5d0edf11c681b82428765
Ari He <ariqhe@gmail.com>
2020-06-16 13:50:12 -0700

    Now press return to go to the next text field.

---
+++ FoodPin/Controller/NewRestaurantControllerTableViewController.swift
@@ -0,0 +1,67 @@
+//
+//  NewRestaurantControllerTableViewController.swift
+//  FoodPin
+//
+//  Created by Quanzhao He on 6/16/20.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class NewRestaurantControllerTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet var nameTextField: RoundedTextField! {
+        didSet {
+            nameTextField.tag = 1
+            nameTextField.becomeFirstResponder()
+            nameTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var typeTextField: RoundedTextField! {
+        didSet {
+            typeTextField.tag = 2
+            typeTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var addressTextField: RoundedTextField! {
+        didSet {
+            addressTextField.tag = 3
+            addressTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var phoneTextField: RoundedTextField! {
+        didSet {
+            phoneTextField.tag = 4
+            phoneTextField.delegate = self
+        }
+    }
+    
+    @IBOutlet var descriptionTextView: UITextView! {
+        didSet {
+            descriptionTextView.tag = 5
+            descriptionTextView.layer.cornerRadius = 5.0
+            descriptionTextView.layer.masksToBounds = true
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let nextTextField = view.viewWithTag(textField.tag+1) {
+            textField.resignFirstResponder() //remove focus
+            nextTextField.becomeFirstResponder() //assign focus
+        }
+        return true
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+}
