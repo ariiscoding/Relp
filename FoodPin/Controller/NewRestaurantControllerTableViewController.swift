a8867a8a24eb9df934a17997fc90a6418dcfed46
Ari He <ariqhe@gmail.com>
2020-06-17 02:04:15 -0700

    Removed separator from "New Restaurant" view

--- FoodPin/Controller/NewRestaurantControllerTableViewController.swift
+++ FoodPin/Controller/NewRestaurantControllerTableViewController.swift
@@ -66,6 +66,9 @@ class NewRestaurantControllerTableViewController: UITableViewController, UITextF
         if let customFont = UIFont(name: "Rubik-Medium", size: 35.0) {
             navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont]
         }
+        
+        //remove separator
+        self.tableView.separatorColor = UIColor.clear
     }
     
     
