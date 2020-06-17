7c0cbcbfb9564c24ea4d809d1d8cce2f550cb47e
Ari He <ariqhe@gmail.com>
2020-06-17 15:29:56 -0700

    Added a data model using Core Data

--- Relp/AppDelegate.swift
+++ Relp/AppDelegate.swift
@@ -33,9 +33,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
 // MARK: - Core Data Stack
-//    lazy var persistentContainer: NSPersistentContainer = {
-//        let container = NSPersistentContainer(name: "Relp")
-//    }
-
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Relp")
+        container.loadPersistentStores(completionHandler: { (StoreDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
