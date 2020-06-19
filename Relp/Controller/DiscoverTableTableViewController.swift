f609b217f004877b3aec1da9468fecf3cd411335
Ari He <ariqhe@gmail.com>
2020-06-19 15:59:22 -0700

    Added basic support for CloudKit with Convenience API.

---
+++ Relp/Controller/DiscoverTableTableViewController.swift
@@ -0,0 +1,132 @@
+//
+//  DiscoverTableTableViewController.swift
+//  Relp
+//
+//  Created by Quanzhao He on 6/19/20.
+//  Copyright © 2020 AppCoda. All rights reserved.
+//
+
+import UIKit
+import CloudKit //to use CloudKit
+
+class DiscoverTableTableViewController: UITableViewController {
+    
+    
+    var restaurants: [CKRecord] = [] //store iCloud results
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.cellLayoutMarginsFollowReadableWidth = true
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        //Configure navigation bar appearance
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        if let customFont = UIFont(name: "Rubik-Medium", size: 40.0) {
+            navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor(red: 231, green: 76, blue: 60), NSAttributedString.Key.font: customFont]
+        }
+        
+        fetchRecordsFromCloud()
+    }
+    
+    // MARK: - iCloud related operations
+    func fetchRecordsFromCloud() {
+        //Fetch data using Convenience API
+        let cloudContainer = CKContainer.default()
+        let publicDatabase = cloudContainer.publicCloudDatabase
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: "Restaurant", predicate: predicate)
+        publicDatabase.perform(query, inZoneWith: nil, completionHandler: {
+            (results, error) -> Void in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let results = results {
+                print("Completed the download of Restaurant data")
+                self.restaurants = results
+                DispatchQueue.main.async {//make sure the UI update is excuted by the main thread 
+                    self.tableView.reloadData()
+                }
+            }
+        })
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurants.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DiscoverCell", for: indexPath)
+
+        //Configure the cell
+        let restaurant = restaurants[indexPath.row]
+        cell.textLabel?.text = restaurant.object(forKey: "name") as? String
+        
+        if let image = restaurant.object(forKey:"image"), let imageAsset = image as? CKAsset {
+            if let imageData = try? Data.init(contentsOf: imageAsset.fileURL!) {
+                cell.imageView?.image = UIImage(data: imageData)
+            }
+        }
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
