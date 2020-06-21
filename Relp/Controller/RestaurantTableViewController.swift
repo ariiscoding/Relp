4a5c553dd91735ee4eba89994d21c75a71df749f
Ari He <ariqhe@gmail.com>
2020-06-20 17:06:34 -0700

    Added partial localization for Chinese (Simplified) and Germany

--- Relp/Controller/RestaurantTableViewController.swift
+++ Relp/Controller/RestaurantTableViewController.swift
@@ -58,7 +58,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
         searchController.obscuresBackgroundDuringPresentation = false
         
         //customize the look of the search bar
-        searchController.searchBar.placeholder = "Search restaurants..."
+        searchController.searchBar.placeholder = NSLocalizedString("Search restaurants...", comment: "Search restaurants...")
         //searchController.searchBar.barTintColor = .white //doesn't work well with dark mode
         searchController.searchBar.backgroundImage = UIImage()
         searchController.searchBar.tintColor = UIColor(red: 231, green: 76, blue: 60)
@@ -128,7 +128,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
     
     //a better implementation to support swipe for more actions
     override func tableView (_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+        let deleteAction = UIContextualAction(style: .destructive, title: NSLocalizedString("Delete", comment: "Delete")) { (action, sourceView, completionHandler) in
             //Delete the row from the data store
             if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
                 let context = appDelegate.persistentContainer.viewContext
@@ -141,8 +141,8 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
             completionHandler(true)
         }
         
-        let shareAction = UIContextualAction(style: .normal, title: "Share") {(action, sourceView, completionHandler) in
-            let defaultText = "Just checking in at " + self.restaurants[indexPath.row] .name!
+        let shareAction = UIContextualAction(style: .normal, title: NSLocalizedString("Share", comment: "Share")) {(action, sourceView, completionHandler) in
+            let defaultText = NSLocalizedString("Just checking in at ", comment: "Just checking in at ") + self.restaurants[indexPath.row] .name!
             
             let activityController: UIActivityViewController
             
@@ -181,7 +181,7 @@ class RestaurantTableViewController: UITableViewController, NSFetchedResultsCont
     //for swipe right action (experimental)
     override func tableView (_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         
-        let checkInAction = UIContextualAction(style: .normal, title:"Check-in") {(action, sourceView, completionHandler) in
+        let checkInAction = UIContextualAction(style: .normal, title:NSLocalizedString("Check-in", comment: "Check-in")) {(action, sourceView, completionHandler) in
             
             let cell = tableView.cellForRow(at: indexPath) as! RestaurantTableViewCell
             self.restaurants[indexPath.row].isVisited = self.restaurants[indexPath.row].isVisited ? false : true
